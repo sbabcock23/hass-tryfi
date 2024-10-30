3bb9b1c981292d7986882d7208e022e364426239
Elliott Walker <elliottwalker@users.noreply.github.com>
2024-10-29 21:04:27 -0700

    update entry setup
    update per entry setup method per
    https://developers.home-assistant.io/docs/config_entries_index/#for-platforms

--- custom_components/tryfi/__init__.py
+++ custom_components/tryfi/__init__.py
@@ -50,10 +50,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     # This creates each HA object for each platform your device requires.
     # It's done by calling the `async_setup_entry` function in each platform module.
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
