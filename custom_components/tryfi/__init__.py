fdfd47ef255d3b7170d17273e4d7767cce4eab54
Ron Heft <ron@ronaldheft.com>
2024-12-22 12:26:28 -0500

    fix: Platforms setup without first refresh data

--- custom_components/tryfi/__init__.py
+++ custom_components/tryfi/__init__.py
@@ -40,10 +40,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = tryfi
 
     coordinator = TryFiDataUpdateCoordinator(hass, tryfi, int(entry.data["polling"]))
-    await coordinator.async_refresh()
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
+    await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
